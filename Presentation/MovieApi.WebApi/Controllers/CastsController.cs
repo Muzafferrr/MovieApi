8c341af45784749eb3024183f76a82671f31f1aa
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2025-04-23 23:44:37 +0300

    mediatr tasarım deseni uygulandı

---
+++ Presentation/MovieApi.WebApi/Controllers/CastsController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IActionResult ListCasts()
+        {
+            var values = _mediator.Send(new GetCastQuery());
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCast(CreateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("Oyuncu ekleme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCast(UpdateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("Oyuncu güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCast(int id)
+        {
+            _mediator.Send(new RemoveCastCommand(id));
+            return Ok("Oyuncu silme işlemi başarılı");
+        }
+
+        [HttpGet("GetCastById")]
+        public IActionResult GetCastById(int id)
+        {
+            var value = _mediator.Send(new GetCastByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
